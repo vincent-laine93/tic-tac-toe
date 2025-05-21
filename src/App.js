90081cdcdf903f416e884b62bea635d9dbee3821
Vincent Laine <vincentlaine.238@gmail.com>
2025-05-21 12:18:37 +0200

    ajout d'une nouvelle fonction principale game incluant le board

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-export default function Board() {
+function Board() {
     const [xIsNext, setXIsNext] = useState(true);
     const [squares, setSquares] = useState(Array(9).fill(null));
 
@@ -56,6 +56,20 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+export default function Game() {
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board />
+            </div>
+            <div className="game-info">
+                <ol>{/*TODO*/}</ol>
+            </div>
+        </div>
+
+    );
 }
 
 function calculateWinner(squares) {
